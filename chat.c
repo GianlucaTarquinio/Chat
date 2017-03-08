0e20e277129bdaa875322b9422777c1e32a84a16
GianlucaTarquinio <awesomegianluca@gmail.com>
2017-03-08 14:02:07 -0500

    Serialization format

--- chat.c
+++ chat.c
@@ -1,13 +1,17 @@
 #include "chat.h"
 
 //returns number of bytes written to buf
-//only serializes the fields neccesary for the type of the message
+//[type, senderNum, dateSeconds, name, '\0',            message,          '\0']
+// 0     4          8            12    12+strlen(name)  13+strlen(name)   13+strlen(name)+strlen(message)
+//maximum strlen(name) is NAME_LEN, maximum strlen(message) is MSG_LEN
 int serializeMessage(Message *m, char *buf) {
 	return 0;
 }
 
 //returns number of bytes read from buf
-//only serializes the fields neccesary for the type of the message
+//[type, senderNum, dateSeconds, name, '\0',            message,          '\0']
+// 0     4          8            12    12+strlen(name)  13+strlen(name)   13+strlen(name)+strlen(message)
+//maximum strlen(name) is NAME_LEN, maximum strlen(message) is MSG_LEN
 int unserializeMessage(Message *m, char *buf) {
 	return 0;
 }
